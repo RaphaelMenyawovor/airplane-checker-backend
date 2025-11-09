3ceec94930576e91cca9aa5ccc0565181368ae2c
Raphael Menyawovor <raphaelmenyawovor@gmail.com>
2025-11-09 23:58:47 +0000

    feat(health): Added db check, and commented the main app file

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import logger from './config/logger.js';
 import userRoutes from './routes/user.routes.js';
 import flightRoutes from './routes/flight.routes.js';
 import bookingRoutes from './routes/booking.routes.js';
+import { PrismaClient, Prisma } from './generated/prisma/client.js';
+const prisma = new PrismaClient();
 
 
 
@@ -19,15 +21,26 @@ app.use(morgan('combined', {
     },
 }));
 
-
+// Routes
 app.use('/api/users', userRoutes);
 app.use('/api/flights', flightRoutes);
 app.use('/api/bookings', bookingRoutes);
 
 
-
+// Healthcheck for server and database
 app.get('/healthcheck', (req: Request, res: Response) => {
     res.status(200).send('Server is healthy');
+});
+
+app.get('/dbcheck', async (req: Request, res: Response) => {
+    try {
+        // The most lightweight query to check database connection
+        await prisma.$queryRaw(Prisma.sql`SELECT 1`);
+        res.status(200).send('Database connection is healthy');
+    } catch (error) {
+        logger.error('Database connection failed:', error);
+        res.status(500).send('Database connection failed');
+    }
 });
 
 
